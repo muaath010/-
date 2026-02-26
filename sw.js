08bbe0280080a014fb8a6f57ccdc6a98a52d6671
muaath010 <maadh0166@moe.om>
2026-02-26 13:45:23 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,16 @@
-const CACHE_NAME = 'naawn-delivery-v1';
+const CACHE_NAME = 'naawn-delivery-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil( caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)) );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith( caches.match(event.request).then(response => response || fetch(event.request)) );
 });
 
 self.addEventListener('activate', event => {
@@ -27,9 +18,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+          if (cacheName !== CACHE_NAME) { return caches.delete(cacheName); }
         })
       );
     })
