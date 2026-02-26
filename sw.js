f0308041f89770d97873871f94f97c206c8c3023
muaath010 <maadh0166@moe.om>
2026-02-26 14:40:54 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'naawn-delivery-v4';
+const CACHE_NAME = 'naawn-delivery-v5';
 const urlsToCache = [
   './',
   './index.html',
@@ -10,15 +10,13 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
